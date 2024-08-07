05633bafb76c7d5e6834ea7280e589220b8f0040
khkpo <khkpoo@gmail.com>
2024-08-07 13:36:06 +0900

    1

--- tesla.py
+++ tesla.py
@@ -1,8 +1,13 @@
 import json
 import openpyxl
 import requests
+import pprint
 
 # Client ID : wJ8lVc_2q173a_Fz6kXj
 # Client Secret : zddOvmeu7V
 header_param={'x-naver-client-id': 'wJ8lVc_2q173a_Fz6kXj', 'x-naver-client-secret':'zddOvmeu7V'}
-url='https://openapi.naver.com/v1/search/cafearticle.json'
+url='https://openapi.naver.com/v1/search/cafearticle.json?query='
+url+='테슬라 업데이트'
+res=requests.get(url,headers=header_param)
+pprint.pprint(res.json())
+
