d3249615b5bcb16b3235ccd1cf607219059d961a
khkpo <khkpoo@gmail.com>
2024-08-13 10:11:20 +0900

    2

--- xl.py
+++ xl.py
@@ -1,9 +1,13 @@
 import openpyxl
 
-def write_xl(filename,sheetname,contents):
+def init_xl(sheetname):
     xl=openpyxl.Workbook()
     xl_sheet=xl.active
-    xl_sheet.name=sheetname
+    xl_sheet.title = sheetname
+def set_header(header):
+    xl_sheet.append(header)
+
+def write_xl(filename,contents):
     for row in contents:
         xl_sheet.append(row)
     xl.save(filename)
